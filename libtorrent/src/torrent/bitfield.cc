4179905d113b8cb9b4c079aaa6bd00a0c5b40ded
stickz <stickman002@mail.com>
2024-07-05 02:04:34 -0400

    fixes

--- libtorrent/src/torrent/bitfield.cc
+++ libtorrent/src/torrent/bitfield.cc
@@ -89,7 +89,7 @@ Bitfield::update() {
   clear_tail();
 
   #if USE_AVX2_POPCOUNT
-  m_set = rak::popcnt_AVX2_lookup(m_data, size_bits());
+  m_set = rak::popcnt_AVX2_lookup(m_data, *end());
   #else
   m_set = 0;
 
